7f51fd85dfd9cbd9a8cd7923c68781cf35a9a21d
Aleks <aleksandro551@bk.ru>
2024-02-18 22:19:24 +0300

    new beginning

--- app/bot.py
+++ app/bot.py
@@ -4,17 +4,35 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from datetime import datetime
+from cfg_reader import config
+from keyboards import *
+from aiogram import F
+from aiogram.types import Message
+from aiogram.enums import ParseMode
+
 
 load_dotenv()
-logging.basicConfig(level=logging.DEBUG)
+#logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-bot = Bot(os.getenv('TOKEN'))
+bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
 dp = Dispatcher()
+dp["started_at"] = datetime.now().strftime("%Y-%m-%d %H:%M")
+admin_ids = [int(admin_id) for admin_id in config.admins.split(',')]
 
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
     await message.answer("started")
+    if message.from_user.id in admin_ids:
+        await message.answer("adm mode")
+
+
+@dp.message(Command("adm_info"))
+async def adm_info(message: types.Message, started_at: str):
+    if message.from_user.id in admin_ids:
+        await message.answer(f"Bot started at {started_at}")
 
 
 async def main():
---
+++ app/keyboards.py
@@ -0,0 +1,24 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+# def main_menu_keyboard():
+#     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+#     button_catalog = KeyboardButton('Каталог')
+#     button_cart = KeyboardButton('Корзина')
+#     button_order_info = KeyboardButton('Мой заказ')
+#     button_contacts = KeyboardButton('Контакты')
+#     button_info = KeyboardButton('О магазине')
+#     keyboard.add(button_catalog, button_cart, button_order_info, button_contacts, button_info)
+#     return keyboard
+#
+#
+# def main_menu_keyboard_admin():
+#     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
+#     button_catalog = KeyboardButton('Каталог')
+#     button_cart = KeyboardButton('Корзина')
+#     button_order_info = KeyboardButton('Мой заказ')
+#     button_contacts = KeyboardButton('Контакты')
+#     button_info = KeyboardButton('О магазине')
+#     button_adm = KeyboardButton('Админ панель')
+#     keyboard.add(button_catalog, button_cart, button_order_info, button_contacts, button_info, button_adm)
+#     return keyboard
