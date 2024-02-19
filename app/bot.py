3c189998008091cb0f255517bfec9efbab9c9067
Aleks <aleksandro551@bk.ru>
2024-02-19 14:44:44 +0300

    v0.0.1

--- app/bot.py
+++ app/bot.py
@@ -6,14 +6,18 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command, CommandObject, CommandStart
 from datetime import datetime
 from cfg_reader import config
-from keyboards import *
+import keyboards as kb
 from aiogram import F
 from aiogram.types import Message
 from aiogram.enums import ParseMode
 
 
 load_dotenv()
+
+# logs for release
 #logging.basicConfig(filename='bot.log', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# debug logs
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 bot = Bot(token=config.bot_token.get_secret_value(), parse_mode="HTML")
@@ -24,15 +28,54 @@ admin_ids = [int(admin_id) for admin_id in config.admins.split(',')]
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("started")
+    await message.answer("started", reply_markup=kb.main_menu_keyboard())
     if message.from_user.id in admin_ids:
-        await message.answer("adm mode")
+        await message.answer("adm mode", reply_markup=kb.main_menu_keyboard_admin())
+
+
+@dp.message(F.text.lower() == 'каталог')
+async def go_to_catalog(message: types.Message):
+    await message.answer('В каталоге пока ничего нет')
+
+
+@dp.message(F.text.lower() == 'корзина')
+async def go_to_catalog(message: types.Message):
+    await message.answer('В корзине пока ничего нет')
+
+
+@dp.message(F.text.lower() == 'мой заказ')
+async def go_to_catalog(message: types.Message):
+    await message.answer('Ваш заказ пуст')
+
+
+@dp.message(F.text.lower() == 'контакты')
+async def go_to_catalog(message: types.Message):
+    await message.answer('По всем вопросам обращаться к t.me/0')
+
+
+@dp.message(F.text.lower() == 'о магазине')
+async def go_to_catalog(message: types.Message):
+    await message.answer(' ')
+
+
+@dp.message(F.text.lower() == 'управление')
+async def go_to_catalog(message: types.Message):
+    if message.from_user.id in admin_ids:
+        await message.answer('Панель управления')
+    else:
+        await message.answer('Я не знаю такой команды', reply_markup=kb.help_inline_keyboard())
 
 
 @dp.message(Command("adm_info"))
 async def adm_info(message: types.Message, started_at: str):
     if message.from_user.id in admin_ids:
         await message.answer(f"Bot started at {started_at}")
+
+
+@dp.message()
+async def unknown_com(message: types.Message):
+    await message.answer('Я не знаю такой команды', reply_markup=kb.help_inline_keyboard())
+
 
 
 async def main():
--- app/keyboards.py
+++ app/keyboards.py
@@ -1,24 +1,38 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
 
 
-# def main_menu_keyboard():
-#     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-#     button_catalog = KeyboardButton('Каталог')
-#     button_cart = KeyboardButton('Корзина')
-#     button_order_info = KeyboardButton('Мой заказ')
-#     button_contacts = KeyboardButton('Контакты')
-#     button_info = KeyboardButton('О магазине')
-#     keyboard.add(button_catalog, button_cart, button_order_info, button_contacts, button_info)
-#     return keyboard
-#
-#
-# def main_menu_keyboard_admin():
-#     keyboard = ReplyKeyboardMarkup(resize_keyboard=True, selective=True)
-#     button_catalog = KeyboardButton('Каталог')
-#     button_cart = KeyboardButton('Корзина')
-#     button_order_info = KeyboardButton('Мой заказ')
-#     button_contacts = KeyboardButton('Контакты')
-#     button_info = KeyboardButton('О магазине')
-#     button_adm = KeyboardButton('Админ панель')
-#     keyboard.add(button_catalog, button_cart, button_order_info, button_contacts, button_info, button_adm)
-#     return keyboard
+def main_menu_keyboard():
+    kb = [
+        [KeyboardButton(text="Каталог")],
+        [KeyboardButton(text="Корзина")],
+        [KeyboardButton(text="Мой заказ")],
+        [KeyboardButton(text="Контакты")],
+        [KeyboardButton(text="О магазине")],
+    ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb,
+                                   resize_keyboard=True,
+                                   input_field_placeholder="Выберите пункт из меню")
+    return keyboard
+
+
+def main_menu_keyboard_admin():
+    kb = [
+        [KeyboardButton(text="Каталог")],
+        [KeyboardButton(text="Корзина")],
+        [KeyboardButton(text="Мой заказ")],
+        [KeyboardButton(text="Контакты")],
+        [KeyboardButton(text="О магазине")],
+        [KeyboardButton(text="Управление")],
+    ]
+    keyboard = ReplyKeyboardMarkup(keyboard=kb,
+                                   resize_keyboard=True,
+                                   input_field_placeholder="Выберите пункт из меню")
+    return keyboard
+
+
+def help_inline_keyboard():
+    kb = [
+        [InlineKeyboardButton(text="/help", callback_data="help")]
+    ]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=kb)
+    return keyboard
