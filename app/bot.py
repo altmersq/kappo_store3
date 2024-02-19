25420cf0890e5829e58e835f607b48be7ebb5eb0
Aleks <aleksandro551@bk.ru>
2024-02-19 03:05:19 +0300

    backup

--- app/bot.py
+++ app/bot.py
@@ -3,7 +3,7 @@ import os
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram.filters import Command, CommandObject, CommandStart
 from datetime import datetime
 from cfg_reader import config
 from keyboards import *
